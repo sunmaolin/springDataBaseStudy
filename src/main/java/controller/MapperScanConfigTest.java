e9d57eb4ce66614242467e34d5db3d58e7f71879
sunmaolin <Mr.sun727@iCloud.com>
2021-02-07 22:54:52 +0800

    Spring整合mybatis   Mapper包扫描MapperScannerConfigure类测试

---
+++ src/main/java/controller/MapperScanConfigTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import mapper.RoleMapper;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pojo.Role;
+
+/**
+ * 测试MapperScanConfig类
+ */
+public class MapperScanConfigTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        RoleMapper roleMapper = context.getBean(RoleMapper.class);
+        Role role = roleMapper.findRoleById(1);
+        System.out.println(role);
+    }
+}
--- src/main/java/mapper/RoleMapper.java
+++ src/main/java/mapper/RoleMapper.java
@@ -1,7 +1,9 @@
 package mapper;
 
+import org.springframework.stereotype.Repository;
 import pojo.Role;
 
+@Repository
 public interface RoleMapper {
     Role findRoleById(int roleId);
 }
